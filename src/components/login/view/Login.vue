85e687887b63f5b51f2345006af04c61ad0124e6
Bob <xiaobo5460@163.com>
2018-09-09 14:09:45 +0800

    1

--- src/components/login/view/Login.vue
+++ src/components/login/view/Login.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="box">
   <header>
     <div><i class="el-icon-close"></i></div>
     <div>登录</div>
@@ -10,8 +10,11 @@
     <div><i class="el-icon-menu"></i><input type="text" placeholder="验证码"><button>获取验证码</button></div>
   </div>
   <footer>
-    <div></div>
-    <div><el-button type="primary" style="background-color:#52cc77">登录</el-button> </div>
+    <div>温馨提示：未注册润教育账号的手机号，登录时会自动完成注册并代表您已同意《润教育用户协议》</div>
+    <div>
+      <el-button type="primary" style="background-color:#52cc77">登录</el-button>
+      <p class="bottomletter">收不到？试试<span>语音验证码</span></p>
+     </div>
   </footer>
 </div>
 </template>
@@ -25,14 +28,18 @@ export default {
 }
 </script>
 <style scoped>
+.box{
+  background-color:#f7f7f7;
+}
   /* 头部设置 */
   header{
-    height:88px;
-    line-height:88px;
+    height:60px;
+    line-height:60px;
     display: flex;
     padding:0 24px;
-    background-color:lightgray;
+    background-color:white;
     justify-content: space-between;
+    margin-bottom:50px;
   }
   header>div:first-child{
     width:100px;
@@ -51,13 +58,18 @@ export default {
     letter-spacing:5px;
   }
   /* 中部设置 */
+  .middle{
+    margin-bottom:20px;
+  }
   .middle>div,.middle>div>input{
    position:relative;
+   background-color:white;
   }
   .middle>div>input{
     width:90%;
     height:60px;
     padding-left: 5px;
+    border:none;
   }
   .middle>div>i{
     font-size:26px;
@@ -65,12 +77,39 @@ export default {
   }
   .middle>div>button{
     width:200px;
-    height:50px;
+    height:45px;
     border:none;
     background-color:#52cc77;
     border-radius:5px;
     position:absolute;
-    top:5px;
+    top:10px;
     right:5px;
+    font-size:18px;
+    color:white;
+    letter-spacing:5px;
    }
+  .middle>div:first-child{
+     border-bottom:1px solid #ccc;
+  }
+   /* 底部设置 */
+   footer>div{
+     width:96%;
+     margin:20px auto;
+   }
+    footer>div:first-child,.bottomletter{
+      color:grey;
+      font-size:14px;
+    }
+    footer>div>button{
+      width:100%;
+      color:white;
+      letter-spacing:5px;
+    }
+    .bottomletter{
+      text-align:right;
+      margin-top:10px;
+    }
+    .bottomletter span{
+      font-size:18px;
+    }
 </style>
